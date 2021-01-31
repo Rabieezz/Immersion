9349f9a198c3b1c07ab36c83d8eef0d72654318a
Rabieezz <rabie.ezzaamari@gmail.com>
2021-01-31 14:46:27 +0100

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,6 @@
 #Author: Rabie (rabiezz@test.com and telephon 055226699 )
 # that is the name that will be used
-name = ARGV.first || "Robot"
-puts " Git Immersion here I come Mr #{name}! "
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
