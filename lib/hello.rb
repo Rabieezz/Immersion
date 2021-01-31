188b20fa58d72cea6056701d220f006216510e0e
Rabieezz <rabie.ezzaamari@gmail.com>
2021-01-31 14:01:25 +0100

    Moved hello.rb to lib

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,4 @@
-#Author: Rabie (rabiezz@test.com)
+#Author: Rabie (rabiezz@test.com and telephon 055226699 )
 # that is the name that will be used
 name = ARGV.first || "Robot"
-puts " Git Immersion here I come Mr #{ARGV.first}! "
+puts " Git Immersion here I come Mr #{name}! "
